e20ed09b9b1ecd62b819a697fc2a81ace2b9bbb7
Sharareh Hasanpourkashani <hasanpour.sh2014@gmail.com>
2024-10-24 14:39:09 +0330

    initial state

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todo from "./Component/Todo"
+
+function App() {
+  
+
+  return (
+    <div className="bg-stone-900 grid py-4 min-h-screen">
+     <Todo/>
+        
+    </div>
+  )
+}
+
+export default App
